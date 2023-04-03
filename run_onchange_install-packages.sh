b365610495b249c8b036f95f42baea3ad3e992b5
Fabio Tea <iam@f4b.io>
2023-04-03 13:51:28 +0200

    save

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-sudo apt install ripgrep
-mkdir -p $HOME/.local/share/fonts
+echo "installing packages..."
+sudo apt-get --quiet --yes install ripgrep asciinema
 
-cd /tmp
-curl -fsSL -O "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/install.sh"
-curl -fsSL -O "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf"
-curl -fsSL -O "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf"
-./install.sh Hack
-./install.sh FiraCode
+echo "installing nerd fonts..."
+mkdir -p "$HOME/.local/share/fonts"
+curl -fsSL -o "$HOME/.local/share/fonts/Hack Regular Nerd Font Complete.ttf" "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack Regular Nerd Font Complete.ttf"
+curl -fsSL -o "$HOME/.local/share/fonts/Hack Regular Nerd Font Complete Mono.ttf" "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack Regular Nerd Font Complete Mono.ttf"
+fc-cache -f
+
+echo "do something else..."
