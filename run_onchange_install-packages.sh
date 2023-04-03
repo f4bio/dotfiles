9e7888e409c6ddd435cf767c64dc1326d2e1498c
Fabio Tea <iam@f4b.io>
2023-04-03 13:54:03 +0200

    save

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -4,9 +4,10 @@ echo "installing packages..."
 sudo apt-get --quiet --yes install ripgrep asciinema
 
 echo "installing nerd fonts..."
-mkdir -p "$HOME/.local/share/fonts"
-curl -fsSL -o "$HOME/.local/share/fonts/Hack Regular Nerd Font Complete.ttf" "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack Regular Nerd Font Complete.ttf"
-curl -fsSL -o "$HOME/.local/share/fonts/Hack Regular Nerd Font Complete Mono.ttf" "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack Regular Nerd Font Complete Mono.ttf"
+LOCAL_FONTS="$HOME/.local/share/fonts"
+mkdir -p "$LOCAL_FONTS"
+curl -fsSL -o "$LOCAL_FONTS/Hack Regular Nerd Font Complete.ttf" "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf"
+curl -fsSL -o "$LOCAL_FONTS/Hack Regular Nerd Font Complete Mono.ttf" "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf"
 fc-cache -f
 
 echo "do something else..."
