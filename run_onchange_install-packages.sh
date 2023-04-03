d1d1314728aba60b13f5c84b0a166c62b85cd510
Fabio Tea <iam@f4b.io>
2023-04-03 15:09:19 +0200

    save

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,17 +1,29 @@
 #!/bin/sh
 
-echo "installing packages..."
+echo "\n\n## ####################"
+echo "## starting onchange install packages..."
+echo "## ################\n\n"
+
+echo "\n## ####################"
+echo "## installing packages..."
+echo "## ################\n"
 sudo apt-get --quiet --yes install zip unzip rar unrar ripgrep asciinema
 
-echo "install 3rd party packages/tools..."
+echo "\n## ####################"
+echo "## install 3rd party packages/tools..."
+echo "## ################\n"
 curl -fsSL -o "$HOME/.local/bin/agg" "https://github.com/asciinema/agg/releases/download/v1.4.0/agg-x86_64-unknown-linux-gnu"
 chmod +x "$HOME/.local/bin/agg"
 
-echo "install zsh plugins..."
+echo "\n## ####################"
+echo "## install zsh plugins..."
+echo "## ################\n"
 git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 
-echo "installing nerd fonts..."
+echo "\n## ####################"
+echo "## installing nerd fonts..."
+echo "## ################\n"
 LOCAL_FONTS="$HOME/.local/share/fonts"
 mkdir -p "$LOCAL_FONTS"
 curl -fsSL -o "/tmp/Hack.zip" "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip"
@@ -24,4 +36,10 @@ cp "/tmp/FiraCode/Fira Code Regular Nerd Font Complete.ttf" "$LOCAL_FONTS/"
 cp "/tmp/FiraCode/Fira Code Regular Nerd Font Complete Mono.ttf" "$LOCAL_FONTS/"
 fc-cache -f
 
-echo "do something else..."
+echo "\n## ####################"
+echo "## do something else..."
+echo "## ################\n"
+
+echo "\n\n## ####################"
+echo "## done onchange install packages..."
+echo "## ################\n\n"
