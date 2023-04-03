027ee3aeb5dde469a8fe21280bfca31bd975e6ee
Fabio Tea <iam@f4b.io>
2023-04-03 13:47:06 +0200

    save

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -2,10 +2,10 @@
 
 sudo apt install ripgrep
 mkdir -p $HOME/.local/share/fonts
-curl -fLo "$HOME/.local/share/fonts/Hack Regular Nerd Font Complete.ttf" "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf"
-curl -fLo "$HOME/.local/share/fonts/Hack Regular Nerd Font Complete Mono.ttf" "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf"
+
 cd /tmp
-git clone https://github.com/ryanoasis/nerd-fonts
-cd nerd-fonts
+curl -fsSL -O "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/install.sh"
+curl -fsSL -O "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf"
+curl -fsSL -O "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf"
 ./install.sh Hack
 ./install.sh FiraCode
