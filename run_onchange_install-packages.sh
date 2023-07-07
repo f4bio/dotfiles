0b16d156d7e68a435f89e42fc8104262ff574301
Fabio Tea <f4bio@users.noreply.github.com>
2023-07-07 23:33:40 +0200

    Update run_onchange_install-packages.sh

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -26,7 +26,9 @@ asdf install java openjdk-20
 asdf global java openjdk-20
 
 ## oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
 ### oh-my-zsh plugins...
 if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
