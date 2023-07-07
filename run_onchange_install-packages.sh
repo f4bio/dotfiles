5a24613ce48ce7c17b0af7a2fadc91bf66ab5620
Fabio Tea <f4bio@users.noreply.github.com>
2023-07-07 23:31:36 +0200

    Update run_onchange_install-packages.sh

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,57 +1,58 @@
 #!/bin/sh
 
-# echo "\n## ####################"
-# echo "## starting onchange install packages..."
-# echo "## ################\n"
+# essential packages
+sudo apt-get --quiet --yes update
+sudo apt-get --quiet --yes install \
+    zip \
+    unzip \
+    rar \
+    unrar \
+    tmux \
+    zsh \
+    git \
+    curl \
+    ripgrep \
+    asciinema \
+    neovim \
+    detox
 
-# echo "\n## ####################"
-# echo "## installing packages..."
-# echo "## ################\n"
-sudo apt-get --quiet --yes install zip unzip rar unrar ripgrep asciinema tmux git curl neovim detox
-
-# echo "\n## ####################"
-# echo "## install 3rd party packages/tools..."
-# echo "## ################\n"
-curl -fsSL -o "$HOME/.local/bin/agg" "https://github.com/asciinema/agg/releases/download/v1.4.0/agg-x86_64-unknown-linux-gnu"
-chmod +x "$HOME/.local/bin/agg"
+# 3rd party packages/tools
+## asdf
 if [ ! -d "$HOME/.asdf" ]; then
-    git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.11.3
+    git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf
 fi
 asdf plugin-add java https://github.com/halcyon/asdf-java.git
 asdf install java openjdk-20
 asdf global java openjdk-20
 
-# echo "\n## ####################"
-# echo "## install zsh plugins..."
-# echo "## ################\n"
+## oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+### oh-my-zsh plugins...
 if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
-    git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+    git clone https://github.com/zsh-users/zsh-autosuggestions \
+        "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 fi
 if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+        "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 fi
 if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
-    git clone https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+    git clone https://github.com/romkatv/powerlevel10k.git \
+        "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 fi
 
-# echo "\n## ####################"
-# echo "## installing nerd fonts..."
-# echo "## ################\n"
-mkdir -p "$HOME/.local/share/fonts"
-curl -fsSL -o "/tmp/Hack.zip" "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip"
-curl -fsSL -o "/tmp/FiraCode.zip" "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/FiraCode.zip"
-unzip -qq -o -d /tmp/Hack /tmp/Hack.zip
-unzip -qq -o -d /tmp/FiraCode /tmp/FiraCode.zip
-cp "/tmp/Hack/Hack Regular Nerd Font Complete.ttf" "$HOME/.local/share/fonts/"
-cp "/tmp/Hack/Hack Regular Nerd Font Complete Mono.ttf" "$HOME/.local/share/fonts/"
-cp "/tmp/FiraCode/Fira Code Regular Nerd Font Complete.ttf" "$HOME/.local/share/fonts/"
-cp "/tmp/FiraCode/Fira Code Regular Nerd Font Complete Mono.ttf" "$HOME/.local/share/fonts/"
-fc-cache -f
-
-# echo "\n## ####################"
-# echo "## do something else..."
-# echo "## ################\n"
-
-# echo "\n## ####################"
-# echo "## done onchange install packages..."
-# echo "## ################\n"
+# nerd fonts
+if [ ! -d "$HOME/.local/share/fonts" ]; then
+    mkdir -p "$HOME/.local/share/fonts"
+    NERD_FONTS_VERSION=$(curl -fsSL https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep "tag_name" | cut -d\" -f4)
+    curl -fsSL -o "/tmp/Hack.zip" "https://github.com/ryanoasis/nerd-fonts/releases/download/$NERD_FONTS_VERSION/Hack.zip"
+    curl -fsSL -o "/tmp/FiraCode.zip" "https://github.com/ryanoasis/nerd-fonts/releases/download/$NERD_FONTS_VERSION/FiraCode.zip"
+    unzip -qq -o -d /tmp/Hack /tmp/Hack.zip
+    unzip -qq -o -d /tmp/FiraCode /tmp/FiraCode.zip
+    cp "/tmp/Hack/Hack Regular Nerd Font Complete.ttf" "$HOME/.local/share/fonts/"
+    cp "/tmp/Hack/Hack Regular Nerd Font Complete Mono.ttf" "$HOME/.local/share/fonts/"
+    cp "/tmp/FiraCode/Fira Code Regular Nerd Font Complete.ttf" "$HOME/.local/share/fonts/"
+    cp "/tmp/FiraCode/Fira Code Regular Nerd Font Complete Mono.ttf" "$HOME/.local/share/fonts/"
+    fc-cache -f
+fi
